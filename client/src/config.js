9436b21e074c470520ae141fc1c6bea9543c4855
dvirsegal <dvir.segal@igentify.com>
2023-02-11 09:49:23 +0200

    add config.js

---
+++ client/src/config.js
@@ -0,0 +1,7 @@
+export default {
+    branch: 'master',
+    environment: 'development',
+    appName: 'Chit Chat App',
+    appDescription: 'A chat room with your friendly bot',
+    socketUrl: 'http://localhost:3000',
+};
